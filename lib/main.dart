439bc6100eee5e7633efdbe4508f422bcde8b050
JeongYunha <yunhadev98@gmail.com>
2022-03-24 21:20:23 +0900

    앱 바, 하단 바 만들어보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // app 구동 시켜줘~ (앱의 메인 페이지를 입력하면 된다)
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp ({ Key? key }) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      // home: Text('안녕') // 텍스트 넣기
+      // home: Icon(Icons.star) // 아이콘 넣기
+      // home: Image.asset('assets/dog.png') // 이미지 넣기(assets 폴더에 대한 정보를 pubspec.yaml에 작성해줘야 한다)
+      // home: Container(width: 50, height: 50, color: Colors.blue) // 네모 박스 넣기
+      // home: Center(
+      //   child: Container(width: 50, height: 50, color: Colors.blue) // child는 항상 쓸 수 있는건 아님(자동완성 잘 확인 할 것)
+      // ) // 내 자식 위젯의 기준점을 중앙으로 설정해준다
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // home:Scaffold( // 상중하로 나누어 주는 위젯
+      //   // appBar: AppBar(), // 상단에 들어갈 위젯
+      //   body: Row( // Row, children[] : 여러 위젯을 가로로 배치하고 싶을 경우
+      //              // Column, children[] : 여러 위젯을 세로로 배치하고 싶을 경우
+      //     mainAxisAlignment: MainAxisAlignment.center , // 정렬 하는 방법(Row에서 MainAxisAllignment는 가로축 정렬 | Column의 경우 세로축 정렬)
+      //     // crossAxisAlignment: CrossAxisAlignment.center, // 정렬 하는 방법(Row에서 MainAxisAllignment는 세로축 정렬 | Column의 경우 가로축 정렬)
+      //     children: [
+      //       Icon(Icons.star),
+      //       Icon(Icons.star),
+      //       Icon(Icons.star),
+      //     ]
+      //   ), // 중간에 들어갈 위젯
+      //   // bottomNavigationBar: BottomAppBar() // 하단에 들어갈 위젯
+      // )
+      home: Scaffold(
+        appBar: AppBar( title: Text('앱임')),
+        body: Text('안녕'),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox( // Container를 사용하면 Lint가 잡아줌 - width, height, child만 쓸 경우 SizedBox 쓰는게 좋음
+                            // Container가 좀 더 무겁기 때문
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
+
+//  MaterialApp() - google 디자인
+// Cupertino 어쩌구 - 아이폰 화면 디자인
+// 그냥 커스터마이징 하고 싶을 경우 - MaterialApp() 사용
