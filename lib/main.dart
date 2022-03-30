bc35d3b5100e557370ac632aa41d46ab7e0a0268
JeongYunha <yunhadev98@gmail.com>
2022-03-30 17:06:39 +0900

    ListView 사용하기

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: Scaffold(
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+          ]
+        )
+      )
     );
   }
 }
 
-//  MaterialApp() - google 디자인
-// Cupertino 어쩌구 - 아이폰 화면 디자인
-// 그냥 커스터마이징 하고 싶을 경우 - MaterialApp() 사용
+// 레이아웃용 위젯들이 너무 길다면 한 단어로 (커스텀 위젯으로) 깔끔하게 축약할 수 있다
+// 커스텀 위젯 만드는 방법 (stless  쓰고 tab 키 누르기)
+// 사용법
+// 1. class 명 작성
+// 2. return 옆에 축약할 레이아웃 넣기 (코드 넣으면 된다)
+// class ShopItem extends StatelessWidget {
+//   const ShopItem({ Key? key }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//           child: Text('안녕'),
+//       );
+//   }
+// }
